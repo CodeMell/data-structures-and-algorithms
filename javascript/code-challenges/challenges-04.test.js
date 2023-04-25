c7dc72bbe04a8082288af8951fc063149ec71747
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-04-25 13:10:15 -0500

    Update challenges-04.test.js

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -35,6 +35,7 @@ For example: 'Cat' would come before 'apple'
 
 const sortNames = (arr) => {
   // Solution code here...
+  return arr.sort();
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -47,6 +48,7 @@ HINT: Beware... JS default is "Lexical" ordering.
 
 const sortNumbers = (arr) => {
   // Solution code here...
+  return arr.sort((a,b)=>a-b);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -59,6 +61,7 @@ HINT: Do it with a custom sort callback, not with using `.reverse()`. ;)
 
 const sortBackwards = (arr) => {
   // Solution code here...
+  return arr.sort((a,b)=>b-a)
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -73,6 +76,7 @@ For example, ['Alphabet', 'Zebra', 'alphabet', 'carrot'] is correctly sorted.
 
 const alphabetize = (arr) => {
   // Solution code here...
+  return arr.sort();
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -90,6 +94,7 @@ Here is an example of the input:
 
 const sortByPrice = (arr) => {
   // Solution code here...
+  return arr.sort((a,b)=>a.price-b.price);
 };
 
 /* ------------------------------------------------------------------------------------------------
